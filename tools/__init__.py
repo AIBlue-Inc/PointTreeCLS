2f7d557469f6a84f08e16f672934a139d6166a4f
Tae Kyung Kim <78451807+snutp@users.noreply.github.com>
2023-09-26 12:24:04 +0900

    Fix util functions in PointGPT

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,3 +3,7 @@ from .runner import test_net
 from .runner_BERT_pretrain import run_net as BERT_pretrain_run_net
 from .runner_BERT_finetune import run_net as BERT_finetune_run_net
 from .runner_BERT_finetune import test_net as BERT_test_run_net
+
+from .runner_GPT_pretrain import run_net as GPT_pretrain_run_net
+from .runner_GPT_finetune import run_net as GPT_finetune_run_net
+from .runner_GPT_finetune import test_net as GPT_test_run_net
