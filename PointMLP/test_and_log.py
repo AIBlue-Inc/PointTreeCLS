6229c010a27972f9688288eaf24b2670659e4763
Tae Kyung Kim <78451807+snutp@users.noreply.github.com>
2024-09-05 08:54:24 +0900

    Feat: 테스트 구동 및 결과 기록 코드 추가

---
+++ PointMLP/test_and_log.py
@@ -0,0 +1,154 @@
+import argparse
+import os
+import datetime
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+from torch.utils.data import DataLoader
+import models as models
+from utils import progress_bar, IOStream
+from data import NifosTree
+import sklearn.metrics as metrics
+from helper import cal_loss
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+import wandb
+
+model_names = sorted(name for name in models.__dict__
+                     if callable(models.__dict__[name]))
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        fmt = '.2f'
+    else:
+        fmt = 'd'
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(cm, annot=True, fmt=fmt, linewidths=.5, square=True, cmap=cmap,
+                xticklabels=classes, yticklabels=classes, cbar=False)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.title(title)
+    plt.savefig(f'{title}.png')
+    plt.close()
+
+def parse_args():
+    parser = argparse.ArgumentParser('testing')
+    parser.add_argument('-c', '--checkpoint', type=str, metavar='PATH',
+                        help='path to save checkpoint (default: checkpoint)')
+    parser.add_argument('--msg', type=str, help='message after checkpoint')
+    parser.add_argument('--batch_size', type=int, default=1, help='batch size in testing')
+    parser.add_argument('--model', default='pointMLP', help='model name [default: pointnet_cls]')
+    parser.add_argument('--num_classes', default=4, type=int, help='number of classes')
+    parser.add_argument('--num_points', type=int, default=1024, help='Point Number')
+    return parser.parse_args()
+
+def test_net(args):
+    wandb.init(project="PointMLP-Test", config=args)
+    wandb.run.name = f"test-{args.model}-{args.num_points}-{args.batch_size}"
+
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    print(f"==> Using device: {device}")
+
+    print('==> Preparing data..')
+    valid_loader = DataLoader(NifosTree(partition='val', num_points=args.num_points), num_workers=4,
+                              batch_size=args.batch_size, shuffle=False, drop_last=False)
+    test_loader = DataLoader(NifosTree(partition='test', num_points=args.num_points), num_workers=4,
+                             batch_size=args.batch_size, shuffle=False, drop_last=False)
+
+    print('==> Building model..')
+    net = models.__dict__[args.model](num_classes=args.num_classes)
+    criterion = cal_loss
+    net = net.to(device)
+    checkpoint_path = os.path.join(args.checkpoint, 'best_checkpoint.pth')
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    net.load_state_dict(checkpoint['net'])
+
+    valid_out = validate(net, valid_loader, criterion, device, args.num_classes, 'Validation')
+    print(f"Validation results: {valid_out}")
+
+    test_out = validate(net, test_loader, criterion, device, args.num_classes, 'Test')
+    print(f"Test results: {test_out}")
+
+    # Log results to wandb
+    for split, results in [('valid', valid_out), ('test', test_out)]:
+        wandb.log({
+            f"{split}_loss": results['loss'],
+            f"{split}_accuracy": results['acc'],
+            f"{split}_balanced_accuracy": results['acc_avg'],
+            f"{split}_f1_score": results['f1'],
+            f"{split}_precision": results['precision'],
+            f"{split}_recall": results['recall'],
+            f"{split}_time": results['time'],
+            "num_points": args.num_points,
+            "batch_size": args.batch_size,
+            f"{split}_predictions": results['pred'],
+            f"{split}_true_labels": results['true'],
+        })
+
+        # Log confusion matrix
+        cm_image = wandb.Image(f'{split.capitalize()} Confusion Matrix.png')
+        wandb.log({f"{split}_confusion_matrix": cm_image})
+
+    wandb.finish()
+
+def validate(net, dataloader, criterion, device, num_classes, split):
+    net.eval()
+    loss = 0
+    true = []
+    pred = []
+    time_cost = datetime.datetime.now()
+    with torch.no_grad():
+        for batch_idx, (data, label) in enumerate(dataloader):
+            data, label = data.to(device), label.to(device).squeeze()
+            data = data.permute(0, 2, 1)
+            logits = net(data)
+            loss += criterion(logits, label).item()
+            preds = logits.max(dim=1)[1]
+            true.append(label.cpu().numpy())
+            pred.append(preds.detach().cpu().numpy())
+            progress_bar(batch_idx, len(dataloader), 'Loss: %.3f'
+                         % (loss / (batch_idx + 1)))
+
+    time_cost = int((datetime.datetime.now() - time_cost).total_seconds())
+    true = np.concatenate(true)
+    pred = np.concatenate(pred)
+
+    # Compute metrics
+    acc = 100. * metrics.accuracy_score(true, pred)
+    acc_avg = 100. * metrics.balanced_accuracy_score(true, pred)
+    f1 = f1_score(true, pred, average='weighted')
+    precision = precision_score(true, pred, average='weighted')
+    recall = recall_score(true, pred, average='weighted')
+
+    # Compute and plot confusion matrix
+    cm = confusion_matrix(true, pred)
+    class_names = ['Densi', 'Koraiensis', 'Larix', 'Obtusa']
+    plot_confusion_matrix(cm, class_names, title=f'{split} Confusion Matrix')
+
+    return {
+        "loss": float("%.3f" % (loss / len(dataloader))),
+        "acc": float("%.3f" % acc),
+        "acc_avg": float("%.3f" % acc_avg),
+        "f1": float("%.3f" % f1),
+        "precision": float("%.3f" % precision),
+        "recall": float("%.3f" % recall),
+        "time": time_cost,
+        "pred": pred.tolist(),
+        "true": true.tolist()
+    }
+
+if __name__ == '__main__':
+    args = parse_args()
+    test_net(args)
---
+++ tools/test_and_log.py
@@ -0,0 +1,132 @@
+import argparse
+import torch
+import wandb
+from tools import builder
+from utils import misc
+import numpy as np
+from torchvision import transforms
+from pointnet2_ops import pointnet2_utils
+from datasets import data_transforms
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('Model')
+    parser.add_argument('--config', type=str, help='path to config file')
+    parser.add_argument('--ckpts', type=str, help='path to checkpoint file')
+    parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+    parser.add_argument('--num_points', type=int, default=2048, help='number of points')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    return parser.parse_args()
+
+
+def test_transforms(points):
+    return data_transforms.PointcloudScaleAndTranslate()(points)
+
+
+def evaluate(base_model, dataloader, criterion, args, split):
+    base_model.eval()
+    preds = []
+    labels = []
+    total_loss = 0.0
+
+    with torch.no_grad():
+        for idx, (_, _, data) in enumerate(dataloader):
+            points = data[0].cuda()
+            label = data[1].cuda()
+
+            points = misc.fps(points, args.num_points)
+            points = test_transforms(points)
+
+            logits, _ = base_model(points)
+            loss = criterion(logits, label)
+            pred = logits.argmax(-1).view(-1)
+
+            preds.extend(pred.cpu().numpy())
+            labels.extend(label.cpu().numpy())
+            total_loss += loss.item()
+
+    preds = np.array(preds)
+    labels = np.array(labels)
+
+    # Calculate metrics
+    acc = (preds == labels).sum() / float(labels.size) * 100.
+    avg_loss = total_loss / len(dataloader)
+    f1 = f1_score(labels, preds, average='weighted')
+    precision = precision_score(labels, preds, average='weighted')
+    recall = recall_score(labels, preds, average='weighted')
+
+    print(f'{split} Accuracy: {acc:.2f}%')
+    print(f'{split} Loss: {avg_loss:.4f}')
+    print(f'{split} F1 Score: {f1:.4f}')
+    print(f'{split} Precision: {precision:.4f}')
+    print(f'{split} Recall: {recall:.4f}')
+
+    # Compute confusion matrix
+    cm = confusion_matrix(labels, preds)
+
+    # Log results to wandb
+    wandb.log({
+        f"{split}_accuracy": acc,
+        f"{split}_loss": avg_loss,
+        f"{split}_f1_score": f1,
+        f"{split}_precision": precision,
+        f"{split}_recall": recall,
+        f"{split}_predictions": preds.tolist(),
+        f"{split}_true_labels": labels.tolist(),
+        f"{split}_confusion_matrix": wandb.plot.confusion_matrix(
+            preds=preds,
+            y_true=labels,
+            class_names=['Densi', 'Koraiensis', 'Larix', 'obtusa']
+        )
+    })
+
+    return acc, avg_loss, f1, precision, recall
+
+
+def test_net(args, config):
+    wandb.init(project="LiDCLS", config=config)
+    wandb.run.name = f"test-{args.num_points}-{args.batch_size}-{args.ckpts}"
+
+    _, valid_dataloader = builder.dataset_builder(args, config.dataset.val)
+    _, test_dataloader = builder.dataset_builder(args, config.dataset.test)
+    base_model = builder.model_builder(config.model)
+    builder.load_model(base_model, args.ckpts)
+
+    if args.gpu:
+        base_model.cuda()
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    print("Evaluating on validation set:")
+    evaluate(base_model, valid_dataloader, criterion, args, "valid")
+
+    print("\nEvaluating on test set:")
+    evaluate(base_model, test_dataloader, criterion, args, "test")
+
+    wandb.log({
+        "num_points": args.num_points,
+        "batch_size": args.batch_size,
+    })
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config = misc.Config.fromfile(args.config)
+    test_net(args, config)
+
+# cd tools
+# python test_and_log.py --config=cfgs/ModelNet_models/PointTransformer_NifosTree4_1k_test.yaml --ckpts=experiments/PointTransformer_NifosTree4_1k_bs16/ModelNet_models/PointBERT-1024-16-1/ckpt-best.pth --num_points=1024 --batch_size=16
+# python test_and_log.py --config=cfgs/ModelNet_models/PointTransformer_NifosTree4_1k_test.yaml --ckpts=experiments/PointTransformer_NifosTree4_1k_bs32/ModelNet_models/PointBERT-1024-32-1/ckpt-best.pth --num_points=1024 --batch_size=32
+# python test_and_log.py --config=cfgs/ModelNet_models/PointTransformer_NifosTree4_2k_test.yaml --ckpts=experiments/PointTransformer_NifosTree4_2k_bs16/ModelNet_models/PointBERT-2048-16-1/ckpt-best.pth --num_points=2048 --batch_size=16
+# python test_and_log.py --config=cfgs/ModelNet_models/PointTransformer_NifosTree4_2k_test.yaml --ckpts=experiments/PointTransformer_NifosTree4_2k_bs32/ModelNet_models/PointBERT-2048-32-1/ckpt-best.pth --num_points=2048 --batch_size=32
+
+# python test_and_log.py --config=cfgs/PointGPT-S/finetune_modelnet_NifosTree4_1k_test.yaml --ckpts=experiments/finetune_modelnet_NifosTree4_1k_bs16/PointGPT-S/PointGPT-1024-16-1/ckpt-best.pth --num_points=1024 --batch_size=16
+# python test_and_log.py --config=cfgs/PointGPT-S/finetune_modelnet_NifosTree4_1k_test.yaml --ckpts=experiments/finetune_modelnet_NifosTree4_1k_bs32/PointGPT-S/PointGPT-1024-32-1/ckpt-best.pth --num_points=1024 --batch_size=32
+# python test_and_log.py --config=cfgs/PointGPT-S/finetune_modelnet_NifosTree4_2k_test.yaml --ckpts=experiments/finetune_modelnet_NifosTree4_2k_bs16/PointGPT-S/PointGPT-2048-16-1/ckpt-best.pth --num_points=2048 --batch_size=16
+# python test_and_log.py --config=cfgs/PointGPT-S/finetune_modelnet_NifosTree4_2k_test.yaml --ckpts=experiments/finetune_modelnet_NifosTree4_2k_bs32/PointGPT-S/PointGPT-2048-32-1/ckpt-best.pth --num_points=2048 --batch_size=32
+
+# python test_and_log.py --cfgs/PointGPT-S/finetune_modelnet_NifosTree4_2k_test.yaml \
+# --ckpts=experiments/finetune_modelnet_NifosTree4_2k_bs16/PointGPT-S/PointGPT-2048-16-1/ckpt-best.pth --num_points=2048 --batch_size=16
