66d55c008db5600d962f7ff6db8807f624f2a1f0
Tae Kyung Kim <78451807+snutp@users.noreply.github.com>
2024-10-09 22:35:05 +0900

    Create run_nifosfinal_gomsol.sh

---
+++ PointView-GCN/PointNet++/run_nifosfinal_gomsol.sh
@@ -0,0 +1,4 @@
+python3 main_nifosfinal_gomsol.py --num_point 1024 --batch_size 16
+python3 main_nifosfinal_gomsol.py --num_point 1024 --batch_size 32
+python3 main_nifosfinal_gomsol.py --num_point 2048 --batch_size 16
+python3 main_nifosfinal_gomsol.py --num_point 2048 --batch_size 32
